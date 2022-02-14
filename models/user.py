96b7e1f05ab1fa8cc4ab3df7bcbaf9a0a8e2faa1
george <test@localhost>
2022-02-14 21:07:11 +0300

    add username column

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class User(Model):
     id = IntegerField(primary_key=True, null=False, index=True, unique=True)
-
+    username = CharField(max_length=100, null=True)
     created_at = DateTimeField(default=datetime.now)
 
     class Meta:
