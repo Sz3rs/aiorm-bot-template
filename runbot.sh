d0669acbfb95397b83037a7c76dba4ea69b7a174
Sz3rs <96138257+Sz3rs@users.noreply.github.com>
2022-07-06 21:26:10 +0400

    Create runbot.sh

---
+++ runbot.sh
@@ -0,0 +1,8 @@
+if [[ $(docker images | grep aiogram-template) ]]; then
+    echo "[ + ] Docker image exists"
+else
+    echo "[ ~ ] Building docker image..."
+    docker build -t aiogram-template .
+fi
+echo '[ ~ ] Running bot...'
+docker run -it --rm -v $PWD:/home/telegram aiogram-template
