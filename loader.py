4664e539a31a45e2813d1afd49ca1ce758380db7
Sz3rs <96138257+Sz3rs@users.noreply.github.com>
2023-02-21 22:07:16 +0400

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from middlewares import UsersMiddleware
+import utils.misc.logging
 from os import getenv
 
 load_dotenv()
